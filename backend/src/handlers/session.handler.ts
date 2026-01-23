545e254c26628f56095c447e55b265994c4d2aaf
Natnael <natieshzed@gmail.com>
2026-01-23 16:37:23 +0300

    implement session and user handlers

---
+++ backend/src/handlers/session.handler.ts
@@ -0,0 +1,60 @@
+import {
+  clearCookie,
+  json,
+  setCookie,
+  status,
+  type CTXBody,
+  type Handler,
+} from "@bepalo/router";
+import { isProduction, type CTXMain } from "../base";
+import type { CTXBodyLogin } from "../routes/api/session.route";
+import {
+  deleteSession as servDeleteSession,
+  createSession as servCreateSessio,
+} from "../services/session.service";
+import { authenticateUser } from "../services/user.service";
+import { Time } from "@bepalo/time";
+import { securityConfig } from "../config";
+
+export const createSession =
+  (): Handler<CTXMain & CTXBodyLogin> => async (_req, ctx) => {
+    const { email, password } = ctx.body;
+    const user = await authenticateUser(email, password);
+    if (!user) {
+      return json({ error: "Invalid login" }, { status: 401 });
+    }
+    const sessionId = await servCreateSessio(user.id);
+    return json(
+      { sessionId, success: true },
+      {
+        status: 200,
+        headers: [
+          setCookie(securityConfig.sessionCookie, sessionId, {
+            path: "/",
+            httpOnly: true,
+            secure: isProduction,
+            expires: Time.after(24).hours.fromNow()._ms,
+            sameSite: "Strict",
+          }),
+        ],
+      },
+    );
+  };
+
+export const deleteSession =
+  (): Handler<CTXMain> =>
+  async (_req, { session }) => {
+    if (!session) return json({ error: "Not logged in" }, { status: 401 });
+    const success = await servDeleteSession(session.id);
+    return json(
+      { success },
+      { status: 200, headers: [clearCookie(securityConfig.sessionCookie)] },
+    );
+  };
+
+export const getCurrentSession =
+  (): Handler<CTXMain> =>
+  (_req, { session }) => {
+    if (!session) return json({ error: "Not logged in" }, { status: 401 });
+    return json({ session, success: true }, { status: 200 });
+  };
